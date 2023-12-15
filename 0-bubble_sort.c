f520badff50b0780a8e0e515ba9a13a62537c507
99omniaashraf <omniaashraf249@gmail.com>
2023-12-14 20:10:46 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,10 +12,10 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
