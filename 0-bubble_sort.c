fbc72212afee84dab1b9f3c9126d4287c4404c4b
99omniaashraf <omniaashraf249@gmail.com>
2023-12-14 20:21:36 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,24 +9,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int tmp, swapped;
+	size_t i, j, tmp = 0;
 
-	for (i = 0; i < size - 1; i++)
-		swapped = 0
-		for (j = 0; j < size - i - 1; j++)
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+		for (j = 0; j < size - i; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j] > array[j + 1] && array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-				swapped = 1;
 				print_array(array, size);
 			}
-		}
-		if (tmp == 0)
-		{
-			break;
 		}
 }
