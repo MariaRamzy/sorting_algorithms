e5b519b5439b65da2560f6bd0162bfd51c57c79d
MariaRamzy <maria_botros94@hotmail.com>
2023-12-16 00:56:15 -0800

    radix

--- sort.h
+++ sort.h
@@ -33,5 +33,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void count_sort_LSD(int *array, size_t size, size_t lsd);
 
 #endif
