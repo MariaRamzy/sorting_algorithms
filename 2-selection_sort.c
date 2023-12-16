b2b78a52c535291372b5f535fefde031df5d6ab9
MariaRamzy <maria_botros94@hotmail.com>
2023-12-16 05:42:10 -0800

    betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,27 +9,27 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int index, tmp, swap = 0;
+size_t i, j;
+int index, tmp, swap = 0;
 
-	if (array == NULL)
-		return;
-	for (i = 0; i < size - 1; i++)
-	{
-		index = i;
-		swap = 0;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[index] > array[j])
-			{
-				index = j;
-				swap += 1;
-			}
-		}
-		tmp = array[i];
-		array[i] = array[index];
-		array[index] = tmp;
-		if (swap != 0)
-			print_array(array, size);
-	}
+if (array == NULL)
+return;
+for (i = 0; i < size - 1; i++)
+{
+index = i;
+swap = 0;
+for (j = i + 1; j < size; j++)
+{
+if (array[index] > array[j])
+{
+index = j;
+swap += 1;
+}
+}
+tmp = array[i];
+array[i] = array[index];
+array[index] = tmp;
+if (swap != 0)
+print_array(array, size);
+}
 }
